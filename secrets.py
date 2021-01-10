fa772bab4e5991f49c3920ebe806b1fd34b12730
Daniel Lemire <github@danlemire.com>
2021-01-10 16:19:25 -0600

    example secrets python file
    the secrects file contains your network ssid and password. Fill in the values for your network to enable wifi.

--- secrets.py
+++ secrets.py
@@ -1,8 +1,16 @@
 # This file is where you keep secret settings, passwords, and tokens!
 # If you put them in the code you risk committing that info or sharing it
-
+# this file is in the repo as an example for you to edit, hence the values of #yourstringhere
 secrets = {
-    'ssid' : 'ENDERSGAME',
-    'password' : '2505158664z',
+    'ssid' : '#yourstringhere',
+    'password' : '#yourstringhere',
     'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
+    'github_token' : '#yourstringhere',
+    'hackaday_token' : '#yourstringhere',
+    'aio_username' : '#yourstringhere',
+    'aio_key' : '#yourstringhere',
+    'openweather_token' : '#yourstringhere',
+    'city_id' : '#yourstringhere',
+    'latitude' : '#yourstringhere',
+    'longitude' : '#yourstringhere'
     }
